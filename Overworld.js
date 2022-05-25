02a6dc336a3af74ac6e893943ee008c332bf0375
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-05-25 14:22:24 -0500

    built cutscene functionality

--- Overworld.js
+++ Overworld.js
@@ -26,7 +26,9 @@ class Overworld {
        this.map.drawLowerImage(this.ctx, cameraPerson);
  
        //Draw Game Objects
-       Object.values(this.map.gameObjects).forEach(object => {
+       Object.values(this.map.gameObjects).sort((a,b) => {
+         return a.y - b.y;
+       }).forEach(object => {
          object.sprite.draw(this.ctx, cameraPerson);
        })
  
@@ -48,5 +50,20 @@ class Overworld {
    this.directionInput.init();
  
    this.startGameLoop();
+ 
+   this.map.startCutscene([
+     { who: "hero", type: "walk",  direction: "down" },
+     { who: "hero", type: "walk",  direction: "down" },
+     { who: "npcA", type: "walk",  direction: "left" },
+     { who: "npcA", type: "walk",  direction: "left" },
+     { who: "npcA", type: "stand",  direction: "up", time: 800 },
+     { who: "hero", type: "walk", direction: "up"},
+     { who: "hero", type: "walk", direction: "up"},
+     { who: "hero", type: "stand", direction: "down"},
+     { who: "npcB", type: "walk", direction: "right"},
+     { who: "npcB", type: "walk", direction: "right"},
+     { who: "npcB", type: "stand", direction: "up"},
+   ])
+ 
   }
  }
